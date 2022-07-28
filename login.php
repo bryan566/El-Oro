45298f7c006a00d39c43100507baef4f19b4429e
bryan <bryan.maldonado.armijos@gmail.com>
2022-07-28 08:45:00 -0500

    login 5

--- login.php
+++ login.php
@@ -64,7 +64,7 @@ if (isset($_POST['login'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie-edge">
-  <title>Login 4</title>
+  <title>Login 5</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css " href="assets/css/style.css">
   <link rel="stylesheet" type="text/css " href="assets/css/css/all.min.css">
