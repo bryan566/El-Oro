22d435c36103e55d7bf2492af8c148f979a3ab01
bryan <bryan.maldonado.armijos@gmail.com>
2022-07-29 16:49:41 -0500

    eliminar formulario en examenes

--- vista/examenes/agregar.php
+++ vista/examenes/agregar.php
@@ -13,40 +13,41 @@
 				<div class="container-fluid">
 
 					<div class="card-body">
-						<form method="POST" autocomplete="off" enctype="multipart/form-data">
+
+						<!-- <form method="POST" autocomplete="off" enctype="multipart/form-data">
 							
 							<div class="row">
-								<!-- <div class="col-sm-12">
+								<div class="col-sm-12">
 									<div class="form-group form-group-default">
 										<label>Fecha</label>
 										<input name="dates" type="date" class="form-control" value="01-01-2020" min="2022-07-20" max="2030-01-01" required placeholder="Ingrese fecha">
 									</div>
-								</div>-->
-								
-								<div class="col-md-6">
-									<div class="form-group form-group-default">
-										<label>Especialidad</label>
-										<select class="form-control" id="continentes" required name="codespe">
-										</select>
-									</div>
 								</div>
 
-								<div class="col-md-6">
-									<div class="form-group form-group-default">
-										<label>Médico</label>
-										<select class="form-control" id="paises" required name="coddoc">
-										</select>
-									</div>
-								</div>
-								
-								<div class="col-sm-12">
-									<div class="form-group form-group-default">
-										<label>Detalle</label>
-										<input name="dates" type="text" class="form-control" required placeholder="Ingrese el detalle">
-									</div>
-
-
+						<div class="col-md-6">
+							<div class="form-group form-group-default">
+								<label>Especialidad</label>
+								<select class="form-control" id="continentes" required name="codespe">
+								</select>
 							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="form-group form-group-default">
+								<label>Médico</label>
+								<select class="form-control" id="paises" required name="coddoc">
+								</select>
+							</div>
+						</div>
+
+						<div class="col-sm-12">
+							<div class="form-group form-group-default">
+								<label>Detalle</label>
+								<input name="dates" type="text" class="form-control" required placeholder="Ingrese el detalle">
+							</div>
+
+
+						</div>
 
 					</div>
 
@@ -59,8 +60,9 @@
 					<button type="submit" name="agregar" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> Guardar Registro</button>
 
 
+				</form>--->
 
-					</form>
+				
 				</div>
 
 			</div>
