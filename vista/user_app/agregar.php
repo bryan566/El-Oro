40729416bd4764c97ed9fc7d44da228ad34b0e72
jacriollo <jacriollo2@utpl.edu.ec>
2022-07-31 22:39:04 -0500

    cambio5

--- vista/user_app/agregar.php
+++ vista/user_app/agregar.php
@@ -1,4 +1,6 @@
 <!-- Agregar Nuevos Registros -->
+<link rel="stylesheet" href="/assets/css/examenes/estilos.css">
+
 <div class="modal fade" id="addRowModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
 
