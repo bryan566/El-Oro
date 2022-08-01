9841728d067b74edeacc26f73f3a8c3acb6b0e0a
jacriollo <jacriollo2@utpl.edu.ec>
2022-07-31 22:55:18 -0500

    cambio58

--- vista/user_app/agregar.php
+++ vista/user_app/agregar.php
@@ -1,5 +1,21 @@
-<!-- Agregar Nuevos Registros -->
-<link rel="stylesheet" href="/assets/css/examenes/estilos.css">
+<!-- Agregar Nuevos Registros 
+<link rel="stylesheet" href="/assets/css/examenes/estilos.css">-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="../user_app/estilos.css">
+
+</head>
+
+<body>
+
+
+
 
 
 
@@ -57,4 +73,24 @@
 		</div>
 	</div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+</body>
+</html>
+
+
 <!-- -->
